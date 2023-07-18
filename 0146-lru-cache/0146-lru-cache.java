b15f8170ece2593cf520b57d1122978a49d1ee75
PankajPandey2001 <136225614+PankajPandey2001@users.noreply.github.com>
2023-07-18 11:20:30 +0530

    Time: 67 ms (65.75%), Space: 120.3 MB (59.20%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,194 +1,143 @@
-/*
-    BASICALLY JIASE IS QUESITON MEIN EK SEQUENCE BNA HUA HAI , AB HUMEIN ADD AUR REMOVE , AUR GET 
-    IS TRAH SE KARNE HAIN KI SAB O(1) MEIN HO , SO IMAGINE FOR THAT WE WILL NEED ONE DEQUEUE , 
-    AND ONE HASHMAP IN WHICH FOR EVEYR KEY HIS ADDRESS AND VALUE IS PREESNT ; 
-
-
-
-*/
-
-// EDGE CASE TO HANDLE IS WHEN , CAPACITY OF THE DATA STRUCTURE IS 1 ; 
-
 class Node
 {
-    int value ; 
-    int key ; 
-    Node prev ; 
-    Node after ;
+    int key = 0 ; 
+    int val = 0 ; 
+    Node prev = null ; 
+    Node next = null  ; 
 
-    public Node(int value , int key)
+    public Node( int val , int key )
     {
-        this.value = value; 
+        this.val = val ; 
         this.key = key ; 
-        prev = null ; 
-        after = null ; 
+
     }
 }
 class LRUCache {
-
-    Node head = null ; 
-    Node tail = null ; 
-    int size = 0 ; 
-    HashMap<Integer,Node> map = new HashMap<>() ; 
+         
+     HashMap<Integer,Node> map = new HashMap<>() ; 
+     Node head = null ;  
+     Node tail = null ; 
+     int capacity = 0 ; 
 
     public LRUCache(int capacity) {
-         size = capacity ; 
-         map  = new HashMap<>() ; 
-         head = null ; 
-         tail = null ; 
+        this.capacity = capacity ; 
     }
     
     public int get(int key) {
          
-         if(map.containsKey(key) == false) 
+         if(map.containsKey(key) == false)
          return -1 ; 
 
 
          Node temp = map.get(key) ; 
 
-          reorder(temp) ; 
-        //  if(temp == head ) // matlab jo last mein add kara h usi ko dubara call hui toh kisi bhi 
-        //  // order mein change nhi hoga ; 
-        //  {
-        //        return temp.value ; 
-        //  }
+         rearrange(temp) ; 
 
-
-        //  if(temp == tail)
-        //  {
-        //      tail.prev.after = null ; 
-        //      tail = tail.prev ; 
-
-        //      temp.prev = null ; 
-        //      temp.after = head ; 
-        //      head = temp ;
-
-        //      return temp.value ; 
-        //  }
-
-        //  if(temp.prev != null)
-        //  {
-        //      temp.prev.after = temp.after ; 
-        //  }
-
-        //  if(temp.after != null)
-        //  {
-        //      temp.after.prev = temp.prev ; 
-        //  }
-
-
-        //  temp.prev = null ; 
-        //  temp.after = head ; 
-
-        //  head = temp ; 
-
-         return temp.value ; 
+         return temp.val ; 
     }
     
     public void put(int key, int value) {
          
-         Node temp = null ; 
+         if(map.containsKey(key))
+         {
+             Node temp = map.get(key) ; 
+             temp.val = value ; 
+             rearrange(temp) ; 
+              return ; 
+         }
 
-        if(map.containsKey(key))
+         Node temp = new Node(value , key ) ; 
+         
+
+         if( map.size() < capacity)
+         {
+             map.put(key , temp) ; 
+             
+             if( head == null)
+             {
+                 head = temp ; 
+                 tail = temp ; 
+             }
+             else
+             {
+                      temp.next = head ; 
+
+                if( head != null)
+               head.prev = temp ; 
+
+
+                 head = temp ;
+             }
+
+            //  temp.next = head ; 
+
+            //  if( head != null)
+            //  head.prev = temp ; 
+
+
+            //  head = temp ; 
+         }
+         else
+         {
+             if( capacity == 1 )
+             {
+                 map.remove(head.key) ; 
+
+                 map.put(key , temp) ; 
+                 head = temp ; 
+                 tail = temp ; 
+             }
+             else
+             {
+                 map.remove(tail.key) ; 
+                 tail = tail.prev ; 
+
+                 tail.next = null ; 
+
+                 map.put(key,temp) ;
+
+                 head.prev = temp ; 
+                 temp.next = head ; 
+
+                 head = temp ; 
+             }
+         }
+
+
+
+    }
+    public void rearrange(Node temp)
+    {
+        if( temp == head)
+        return ; 
+
+
+        if( temp == tail )
         {
-            temp = map.get(key) ; 
+            tail.prev.next = null ; 
+            tail = tail.prev ; 
 
-            temp.value = value ; 
+            temp.next = head ; 
+            head.prev = temp; 
+            head = temp ;
 
-            reorder(temp) ; 
             return ; 
         }
 
 
 
-        if(map.size() == size)
-        {
-            // then remove tail from the linked list ; 
+        temp.prev.next  = temp.next ; 
 
-            int tempKey = tail.key ; 
-
-            map.remove(tempKey) ;  // yahaan galti kar di thi 
-             
-             if(tail.prev != null)
-             {
-                 tail.prev.after = null ; 
-                  tail = tail.prev ;
-             }
-             else
-             {
-
-                 // else block mein tab hi aayenge jab size 1 hoga ; 
-
-                 // NICHE JO HEAD AND TAIL KO NULL KARA H WO KARNA BHOT JARURI HAI ; 
-                 // KYONKI JAB CAPACITY 1 HOGI , TOH JAISE WO NODE NIKALENGE ; 
-                 // TOH SIZE 0 HO JAYGA ISLYE HEAD AUR TAIL KO DUBARA NULL KARNA JARURI H ; 
-                 head = null ; 
-                 tail = null ;
-             }
-             
-
-            
-        }
+        temp.next.prev = temp.prev ; 
 
 
-         
+        temp.next = head ; 
+        head.prev = temp ; 
 
-         temp = new Node(value , key) ;
-         map.put(key , temp) ; 
-         
-         if(head == null || map.size() == 0) // ye map.size() ==0 edge case ke liye likha h. ; 
-         {
-             head = temp ;
-             tail = temp ; 
-
-             return ; 
-         }
-         temp.after = head ;
-         head.prev = temp ;  
-         head = temp ; 
-    }
-    public void reorder( Node temp)
-    {
-           
-         if(temp == head ) // matlab jo last mein add kara h usi ko dubara call hui toh kisi bhi 
-         // order mein change nhi hoga ; 
-         {
-               return  ; 
-         }
+        head = temp ; 
 
 
-         if(temp == tail)
-         {
-             tail.prev.after = null ; 
-             tail = tail.prev ; 
 
-             temp.prev = null ; 
-             temp.after = head ; 
-             head.prev = temp ; 
-             head = temp ;
-
-             return ; 
-         }
-
-         if(temp.prev != null)
-         {
-             temp.prev.after = temp.after ; 
-         }
-
-         if(temp.after != null)
-         {
-             temp.after.prev = temp.prev ; 
-         }
-
-
-         temp.prev = null ; 
-         temp.after = head ; 
-         head.prev = temp ; 
-
-         head = temp ; 
-
-
-         return ; 
     }
 }
 
